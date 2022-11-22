307d3a27596d22c49697f595a0e4040a7e5a4c4b
Edward Ingram <edward.ingram23@gmail.com>
2022-11-22 13:11:22 -0800

    update

--- roles/vault/files/pki-policy.sh
+++ roles/vault/files/pki-policy.sh
@@ -1,3 +1,5 @@
+vault policy write pki - <<EOF
 path "pki*"                      { capabilities = ["read", "list"] }
 path "pki/sign/local-dot-lan"    { capabilities = ["create", "update"] }
 path "pki/issue/local-dot-lan"   { capabilities = ["create"] }
+EOF
